1a1d9ec3a7e0ad6e643b7a2fc900e5da1e83b578
animeshdeepak <animeshdeepak1@gmail.com>
2022-01-23 00:55:02 +0530

    camera permission

--- app/src/main/java/com/example/aadhaarpannoocr/MainActivity.kt
+++ app/src/main/java/com/example/aadhaarpannoocr/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.example.aadhaarpannoocr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.aadhaarpannoocr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                val data: Intent? = result.data
+                val bitmap = data?.extras?.get("data") as Bitmap
+            }
+
+        setUpObserver()
+        askCameraPermission()
+        binding.captureImageBtn.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(intent)
+    }
+
+    private fun setUpObserver() {
+        viewModel.text.observe(this) {
+            // TODO
+        }
+    }
+
+    private fun askCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_CODE
+            )
+        }
+    }
+
+    companion object {
+        const val TAG = "TAG"
+        const val REQUEST_CAMERA_CODE = 100
     }
 }
